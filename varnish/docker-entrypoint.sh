86379e23770a28282df9891dc1cdd895540fcd8a
Giovanbattista Amato <giovanbattista.amato@linux.com>
2020-03-31 12:27:48 +0200

    Add varnish, elasticsearch, rabbitmq

---
+++ varnish/docker-entrypoint.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -euo pipefail
+
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
+
+# allow configuration to be specified via environment variables
+envs=(
+		BACKEND_HOST
+		PURGE_HOST
+	)
+
+haveConfig=
+for e in "${envs[@]}"; do
+	file_env "$e"
+	if [ -z "$haveConfig" ] && [ -n "${!e}" ]; then
+		haveConfig=1
+	fi
+done
+
+# set backend host if we have environment-supplied configuration value
+# otherwise use 'localhost'
+: "${BACKEND_HOST:=localhost}"
+echo >&2 "Setting '$BACKEND_HOST' as backend host"
+sed -i -e "s/BACKEND_HOST/$BACKEND_HOST/g" /etc/varnish/default.vcl
+
+# set purge host if we have environment-supplied configuration value
+# otherwise use 'localhost'
+# TODO: allow to specify access list (multiple hosts)
+: "${PURGE_HOST:=localhost}"
+echo >&2 "Setting '$PURGE_HOST' as purge host"
+sed -i -e "s/PURGE_HOST/$PURGE_HOST/g" /etc/varnish/default.vcl
+
+exec "$@"
